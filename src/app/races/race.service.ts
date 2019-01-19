2937929c528503689ccb2124172cc110c0ce7d64
Adrian Puscasu <apu@f5it.ro>
2019-01-20 01:49:46 +0200

    - fixes the map related error

--- src/app/races/race.service.ts
+++ src/app/races/race.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Race } from '../race/race';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class RaceService {
@@ -10,10 +10,10 @@ export class RaceService {
 
   getRaces() {
     return this.http.get('assets/races.json')
-          .map(response => <Race[]>response.json().racesData);
+          .pipe(map(response => <Race[]>response.json().racesData));
   }
   getRace(raceId: string) {
     return this.getRaces()
-    .map( races => races.find( race => race.id.toString() === raceId));
+    .pipe(map( races => races.find( race => race.id.toString() === raceId)));
   }
 }
